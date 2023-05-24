8434e351a3136ab84af0e1963cc797cea4619839
vrcjio <132543591+vrcjio@users.noreply.github.com>
2023-05-24 18:26:25 +0530

    part3

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,40 @@
-import React from 'react'
+'use client'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
-const loading = () => {
+export default function LinearBuffer() {
+  const [progress, setProgress] = React.useState(0);
+  const [buffer, setBuffer] = React.useState(10);
+
+  const progressRef = React.useRef(() => {});
+  React.useEffect(() => {
+    progressRef.current = () => {
+      if (progress > 100) {
+        setProgress(0);
+        setBuffer(10);
+      } else {
+        const diff = Math.random() * 10;
+        const diff2 = Math.random() * 10;
+        setProgress(progress + diff);
+        setBuffer(progress + diff + diff2);
+      }
+    };
+  });
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      progressRef.current();
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div><center>Loading...</center></div>
-  )
+    <Box sx={{ width: '100%' , position:"fixed", top:"50%", left:"50%", transform:"translate(-50%,-50%)"}}>
+      <LinearProgress variant="buffer" value={progress} valueBuffer={buffer} />
+    </Box>
+  );
 }
-
-export default loading
